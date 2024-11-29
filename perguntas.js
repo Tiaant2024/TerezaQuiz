a1422f28dd47bbc7bd82f56e3316b5af60be03cb
Tiaant2024 <165724775+Tiaant2024@users.noreply.github.com>
2024-11-29 13:57:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Oque é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A Capital da França é Paris'
+)
+
+criaCartao(
+    'Química',
+    'O que é Química?',
+    'É a ciência que estuda a estrutura da materia e suas transformações.'
+)
+
+criaCartao(
+    'Escola',
+    'Qual é o melhor professor?',
+    'Tiago A Lima.'
+)
